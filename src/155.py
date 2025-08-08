e74a1308bb96cebe0e6764a5f7d81b22a330d770
jounger <nguyenvanan.12a8@gmail.com>
2025-08-09 00:06:09 +0700

    solve(155): Min Stack

--- src/155.py
+++ src/155.py
@@ -14,14 +14,62 @@ class MinStack:
 
     def pop(self) -> None:
         self.stack.pop()
-        self.min = self.stack[len(self.stack) -
-                              1][1] if len(self.stack) > 0 else None
+        self.min = self.stack[-1][1] if len(self.stack) > 0 else None
 
     def top(self) -> int:
-        return self.stack[len(self.stack) - 1][0]
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.stack[len(self.stack) - 1][1]
+        return self.stack[-1][1]
+
+
+class MinStack2:
+    def __init__(self):
+        self.list = LinkedList()
+
+    def push(self, val: int) -> None:
+        self.list.push(val)
+
+    def pop(self) -> None:
+        self.list.pop()
+
+    def top(self) -> int:
+        if self.list.head == None:
+            raise ValueError("No value")
+        return self.list.head.val
+
+    def getMin(self) -> int:
+        if self.list.head == None:
+            raise ValueError("No value")
+        return self.list.head.min
+
+
+class LinkedNode:
+    def __init__(self, val: int, min_val: int):
+        self.val = val
+        self.min = min_val
+        self.next: LinkedNode | None = None
+        self.prev: LinkedNode | None = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head: LinkedNode | None = None
+
+    def pop(self):
+        if self.head == None:
+            return
+        self.head = self.head.prev
+
+    def push(self, val):
+        if self.head == None:
+            self.head = LinkedNode(val, val)
+            return
+
+        node = LinkedNode(val, min(self.head.min, val))
+        node.prev = self.head
+        self.head.next = node
+        self.head = self.head.next
 
 
 # Your MinStack object will be instantiated and called as such:
