a0a26a9557750b7651d9fa3abb1dc10ef542ecd9
jounger <nguyenvanan.12a8@gmail.com>
2025-08-10 03:24:04 +0700

    solve(739): Daily Temperatures

--- src/739.py
+++ src/739.py
@@ -4,23 +4,17 @@
 from typing import List
 
 
-# Time Limit Exceeded 36/48
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = []
         out = [0] * len(temperatures)
         for i, temp in enumerate(temperatures):
-            stack.append((temp, i))
             s = len(stack) - 1
             while s >= 0:
-                m = temp - stack[s][0]
-                if m < 0:
+                if temp <= stack[s][0]:
                     break
-                if m == 0:
-                    s -= 1
-                    continue
-                p = stack[s][1]
-                out[p] = i - p
+                out[stack[s][1]] = i - stack[s][1]
                 stack.pop(s)
                 s = len(stack) - 1
+            stack.append((temp, i))
         return out
