cae04df9411ba96cd8eb806c81e6ac9576f31ff4
jounger <nguyenvanan.12a8@gmail.com>
2025-08-12 13:47:47 +0700

    solve(167): Two Sum II - Input Array Is Sorted

---
+++ src/167.py
@@ -0,0 +1,27 @@
+# 167. Two Sum II - Input Array Is Sorted
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int, i: int, j: int) -> int:
+        while i <= j:
+            mid = (i + j) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                j = mid - 1
+            else:
+                i = mid + 1
+        return -1
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        length = len(numbers)
+        for i, num in enumerate(numbers):
+            find = target - num
+            found = self.search(numbers, find, i + 1, length - 1)
+            if found == -1:
+                continue
+            return [i+1, found+1]
+        raise ValueError("Not found")
