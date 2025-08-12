242600f0807cd419b8de30eb4d0e7205c916caea
jounger <nguyenvanan.12a8@gmail.com>
2025-08-13 02:23:41 +0700

    solve(15): 3Sum

--- src/15.py
+++ src/15.py
@@ -8,17 +8,31 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums = sorted(nums)
         out = set()
-        for i, num in enumerate(nums):
+        i = 0
+        while i < len(nums) - 1:
+            num = nums[i]
+            if num > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                i += 1
+                continue
             j = i + 1
             k = len(nums) - 1
             while j < k:
                 sum3 = num + nums[j] + nums[k]
-                if sum3 == 0:
-                    out.add((num, nums[j], nums[k]))
-                if j == k - 1 and i == j - 1:
-                    break
-                if sum3 > 0:
+                if sum3 >= 0:
+                    if sum3 == 0:
+                        out.add((num, nums[j], nums[k]))
+                    if j == k - 1 and i == j - 1:
+                        break
                     k -= 1
-                else:
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+                    if k < 0:
+                        break
+                    continue
+                j += 1
+                while j < k and nums[j] == nums[j - 1]:
                     j += 1
+            i += 1
         return list(out)
